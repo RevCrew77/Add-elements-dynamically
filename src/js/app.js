41af4f02c4e0be81d7a8638c6a2303f2e3d70e5f
RevCrew77 <kalin_tonev77@abv.bg>
2022-01-20 19:24:12 +0200

    dai da vi viim

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,16 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  function addArticle() {
+    const myDiv = document.getElementById('main')
+
+    const artc = document.createElement('article')
+    artc.className = "message";
+    artc.innerHTML = "Hello there!"
+    
+    myDiv.append(artc*5)
+  }
 
   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  button.addEventListener("click", addArticle());
 });
