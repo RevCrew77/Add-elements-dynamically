ef66143d86de5d1f867568d9b906e329205f3d01
RevCrew77 <kalin_tonev77@abv.bg>
2022-01-20 19:45:24 +0200

    asdsa

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,11 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   function addArticle() {
-    const myDiv = document.getElementById('main')
-
-    const artc = document.createElement('article')
+    const artc = document.createElement("article")
     artc.className = "message";
     artc.innerHTML = "Hello there!"
     
-    myDiv.append(artc*5)
+    document.body.appendChild((artc)*5)
   }
 
   const button = document.querySelector(".button");
